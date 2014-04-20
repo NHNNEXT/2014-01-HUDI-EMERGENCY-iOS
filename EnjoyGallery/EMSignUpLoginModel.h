7fa04e2246c54a0b8518d40c4c8f4dd472e0a380
youngnam <utopianami@gmail.com>
2014-04-20 19:40:12 +0900

    signup/loginmodel

---
+++ EnjoyGallery/EMSignUpLoginModel.h
@@ -0,0 +1,13 @@
+//
+//  EMSignUpLoginModel.h
+//  EnjoyGallery
+//
+//  Created by LeeYoungNam on 4/20/14.
+//  Copyright (c) 2014 EntusApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EMSignUpLoginModel : NSObject
+
+@end
---
+++ EnjoyGallery/EMSignUpLoginModel.m
@@ -0,0 +1,13 @@
+//
+//  EMSignUpLoginModel.m
+//  EnjoyGallery
+//
+//  Created by LeeYoungNam on 4/20/14.
+//  Copyright (c) 2014 EntusApps. All rights reserved.
+//
+
+#import "EMSignUpLoginModel.h"
+
+@implementation EMSignUpLoginModel
+
+@end
